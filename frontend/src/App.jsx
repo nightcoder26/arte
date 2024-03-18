71c9c48e4853bbc2176ab9ef6506b1da92b5305f
Knight <77844703+nightcoder26@users.noreply.github.com>
2024-03-18 23:00:36 +0530

    Create Pages for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import Landing from "./pages/Landing";
+import Assessments from "./pages/Assessments";
+import Chats from "./pages/Chats";
+import Connect from "./pages/Connect";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import MyProfile from "./pages/MyProfile";
+import Settings from "./pages/Settings";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div  className="">Home</div>} />
-          <Route path="/about" element={<div>About</div>} />
-          <Route path="/contact" element={<div>contact</div>} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/assessments" element={<Assessments />} />
+          <Route path="/chats" element={<Chats />} />
+          <Route path="/connect" element={<Connect />} />
+          <Route path="/myprofile" element={<MyProfile />} />
+          <Route path="/settings" element={<Settings />} />
         </Routes>
       </BrowserRouter>
     </>
