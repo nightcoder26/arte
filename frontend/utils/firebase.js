a73ae057ae9aca0b6b31e3d6f8f62e6f34def0db
rahul_thakur <rahulthakur.developer@gmail.com>
2024-06-02 16:18:04 +0530

    configure firebase

---
+++ frontend/utils/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtNfPeCLrHcP19reA9SdoWJEA46tYHF-w",
+  authDomain: "arte-a70c5.firebaseapp.com",
+  projectId: "arte-a70c5",
+  storageBucket: "arte-a70c5.appspot.com",
+  messagingSenderId: "917779127062",
+  appId: "1:917779127062:web:6f9aefa1e4a762f7fb8e37"
+};
+
+
+export const app = initializeApp(firebaseConfig);
